be446765c193e915abc982f311874370182bc140
gislend <78209996+gislend@users.noreply.github.com>
2021-01-30 19:03:57 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.lightLevel() > 150) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.lightLevel() > 80) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
